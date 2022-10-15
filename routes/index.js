165f2fb3f2fcfa0c5fc25d1f612ea30e87a4617a
SUDA Hiroshi <suda@net.it-chiba.ac.jp>
2022-10-15 10:42:20 +0900

    エントリーページを修正

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', id: 'foo' });
+  res.render('chat', { title: 'Chat by socket.io' });
 });
 
 router.get('/chat', function(req, res, next) {
